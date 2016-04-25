7e3fbcc14ae09e0a1fbd21fbfbd7013bdac1bf2c
damans <dpsingh_36@yahoo.com>
2016-04-25 18:10:23 +0530

    Controllers added to the repo

---
+++ controllers/Task.js
@@ -0,0 +1,58 @@
+
+
+var task=require('../models/task');
+var taskHistory= require('../models/Taskhistory');
+var projectCtrl=require('./ProjectController');
+exports.postTasks=function(req,res){
+/*  title: { type: String,  required: true},
+     assignedto: { type: String, required: true},
+     state: {type: String, required: true},
+     priority: {type: String, required: true},
+     description: {type: String},
+     history:[Array]*/
+  var newtask=new task();
+  newtask.projectid=req.body.projectid;
+  newtask.title=req.body.title;
+  newtask.assignedto=req.body.assignedto;
+  newtask.state=req.body.state;
+  newtask.priority=req.body.priority;
+  newtask.description=req.body.description;
+  newtask.history=req.body.history;
+  var project=projectCtrl.findProjectById(newtask.projectid);
+ // console.log(project);
+  if(true)//if(project!=undefined)
+  {
+
+   newtask.save(function(err){
+   	if(err)
+   	{
+
+   		res.send(err);
+   	}
+   	else
+   	{
+       res.json({success:true, message:'task saved successfully.'});
+
+   	}
+   });
+ }
+ else
+
+ {
+
+    res.json({success: false, message: 'Project does not existis'});
+ }
+
+};
+
+exports.getTasks=function(req,res){
+
+	task.find({}, function(err, tasks){
+
+		if(err)
+		{
+			res.send(err);
+		}
+		res.json(tasks);
+	});
+};
---
+++ controllers/user.js
@@ -0,0 +1,162 @@
+var userObj=require('../models/user');
+var config = require('../config');
+ var passwordHash = require('password-hash');
+ var jwt    = require('jsonwebtoken'); 
+ 
+
+ 
+exports.postUsers=function(req,res){
+
+
+var user=new userObj();
+     
+     if(!req.body.id) 
+     {
+     	var hashed=passwordHash.generate(req.body.password);
+     	user.email=req.body.email;
+     user.password=hashed;
+     user.name=req.body.name;
+     user.role=req.body.role;
+     user.save(function (err){
+
+		if(err) {
+
+         return res.status(404).json({message: 'Error saving user data: '+ err.message})
+			}
+
+		//console.log('User saved successfully');
+
+		res.json({ success: true, message: 'User saved successfully' });
+
+	});
+}
+  else
+  {
+
+  	console.log('update user');
+  	var obj=new Object();
+  	if(req.body.password)
+  		obj.password=passwordHash.generate(req.body.password);
+  	if(req.body.name)
+  		obj.name=req.body.name;
+  	if(req.body.email)
+  		obj.email=req.body.email;
+  	if(req.body.role)
+  		obj.role=req.body.role;
+
+    console.dir(obj);
+
+    /*var hashed=passwordHash.generate(req.body.password);
+     	user.email=req.body.email;
+     user.password=hashed;
+     user.name=req.body.name;
+     user.role=req.body.role;*/
+     userObj.update({_id: req.body.id}, { $set: obj}, function(err,result){
+            
+            if(err)
+
+            	return  res.status(404).json({
+        message: 'Incorrect User data : ' +err.message
+      });
+
+            	res.json({success: true, message: "User updated successfully"});
+
+     });
+     //comments
+    /* userObj.findById({id: req.body.id}, function(err, user){
+
+        if(err)
+        {
+        	 throw err;
+        }
+        console.log(user);
+
+          
+
+     });*/
+     /*project.findById({id: id},function(err,prj){
+
+ 	if(err){
+   			console.log(err);
+   			return null;
+   		}
+   		console.log("Inside Controller : "+ project.name);
+   		return prj;
+   });*/
+
+  }
+};
+
+exports.getUsers=function(req,res){
+
+
+	/* email: { type: String, unique: true, required: true},
+     password: { type: String, required: true},
+     name: {type: String, required: true},
+     role: {type: Array, required: true}*/
+
+     userObj.find({}, function(err, users){
+
+		if(err)
+		{
+			return res.status(404).json({message: "Error getting user data: "+ err.message});
+		}
+		res.json(users);
+	});
+};
+
+exports.AuthenticateUser=function(req,res){
+
+   if(req.get('AdminUser'))
+   {
+      console.log(config.adminuser + ","+  config.password);
+   	  if(req.body.username==config.adminuser && req.body.password == config.password){
+
+   	  	var token=jwt.sign({name:config.adminuser, ip:req.connection.remoteAddress},config.secret, {
+    					expiresInMinutes:1440 // expires in 24 hours
+    			});
+
+   	  	res.json({
+    				success: true,
+    				message: 'Enjoy your token!',
+    				token: token
+    			});
+   	  }
+   }
+  else{
+  // -- end of admin user
+    userObj.findOne({
+    	email : req.body.username
+    }, function(err, user){
+
+    	if(err) throw err;
+    	if(!user){
+
+    		res.json({ success: false, message: 'Authentication failed. User not found'});
+
+    	}else if (user){
+
+    		//check if password matchese.
+
+    		if(!passwordHash.verify(req.body.password,user.password))
+    		{
+    			res.json({sucess: false, message: 'Authentication failed. Wrong password'});
+    		}else{
+    				console.log(req.connection.remoteAddress);
+    			var token=jwt.sign({name:user.name, ip:req.connection.remoteAddress}, config.secret, {
+    					expiresInMinutes:1440 // expires in 24 hours
+    			});
+
+    			res.json({
+    				success: true,
+    				message: 'Enjoy your token!',
+    				token: token
+    			});
+    		}
+
+
+    	}
+    });
+}
+
+};
