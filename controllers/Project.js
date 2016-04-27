ca930f2e70ecb0173f1bf8b9e2838de09906e764
damans <dpsingh_36@yahoo.com>
2016-04-27 14:53:36 +0530

    Further changes

--- controllers/Project.js
+++ controllers/Project.js
@@ -1,9 +1,22 @@
-
+/*
+   Project controler for project related operations
+   /*
+    name: { type: String, required: true},
+      	description: {type: String, required: false},
+      	technology: {type: Array, required : false},
+      	startdate: {type: Date, required: true},
+      	duration: {type: Number, required: false},
+      	expectedenddate: {type: Date, required:false},
+      	actualcompletiondate: {type: Date, required: false}
+   */
+*/
 var mongoose=require('mongoose');
 var project=require('../models/project');
 var ObjectId = mongoose.Types.ObjectId
 exports.postProject=function(req,res){
 
+  if(!req.body.id)
+  {
   var newProject=new project();
   newProject.name=req.body.name;
   newProject.startdate=req.body.startdate;
@@ -19,8 +32,27 @@ exports.postProject=function(req,res){
        res.json({success:true, message:'project saved successfully.'});
 
    	}
-   });
 
+   });
+   }
+   else
+   {
+      var newProject=new project();
+       newProject.name=req.body.name;
+       newProject.description= req.body.description;
+       newProject.technology=req.body.technology;
+       newProject.actualcompletiondate=req.body.actualcompletiondate; 
+  newProject.startdate=req.body.startdate;
+  newProject.duration=req.body.duration;
+     //updating project
+   	project.update({_id: req.body.id},{$set: newProject}, function(err,result){
+
+   			if(err)
+   				res.status(400).json({message: 'Incorrect project data.  '+ err.message});
+
+   			res.json({success:true, message: 'Project updated successfully'});
+   	});
+   }
 };
 
 exports.getProjects=function(req,res){
